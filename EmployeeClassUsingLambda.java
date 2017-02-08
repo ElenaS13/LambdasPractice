06b48d789e1adfdd445a66f509bf6d05d0b2435a
Elena Sidyakina <elenasidyakina@gmail.comm>
2017-02-07 19:42:08 -0800

    added methods to main

--- EmployeeClassUsingLambda.java
+++ EmployeeClassUsingLambda.java
@@ -81,9 +81,58 @@ public class EmployeeClassUsingLambda {
 		System.out.printf("%nEmployee names in order by last name then first name:%n");
 		list.stream()
 			.sorted(lastThenFirst)
-			
 			.forEach(System.out::println);
 		
-	}
+		System.out.printf("%nEmployees by department:%n");
+		Map<String, List<Employee>> groupedByDepartment =
+		
+		list.stream()
+		   .collect(Collectors.groupingBy(Employee::getDepartment));
+groupedByDepartment.forEach(
+		   
+		 (department, employeesInDepartment) ->
+		   {
+		System.out.println(department);
+		employeesInDepartment.forEach(
+		 employee -> System.out.printf("   %s%n", employee));
+		  }
+		   );
+
+System.out.printf("%nCount of Employees by department:%n");
+Map<String, Long> employeeCountByDepartment =
+   list.stream()
+   .collect(Collectors.groupingBy(Employee::getDepartment,
+  
+Collectors.counting()));
+    employeeCountByDepartment.forEach(
+(department, count) -> System.out.printf(
+  "%s has %d employee(s)%n", department, count));
+    
+ // sum of Employee salaries with DoubleStream sum method
+    System.out.printf(
+    "%nSum of Employees' salaries (via sum method): %.2f%n",
+   list.stream()
+    
+    .mapToDouble(Employee::getSalary)
+  .sum());
+// calculate sum of Employee salaries with Stream reduce method
+System.out.printf(
+ "Sum of Employees' salaries (via reduce method): %.2f%n",
+list.stream()
+.mapToDouble(Employee::getSalary)
+
+    .reduce(0, (value1, value2) -> value1 + value2)
+    );
+     // average of Employee salaries with DoubleStream average method
+ System.out.printf("Average of Employees' salaries: %.2f%n",
+list.stream()
+ .mapToDouble(Employee::getSalary)
+
+    .average()
+ .getAsDouble());
+} // end main
+// end class ProcessingEmployees
+		
+
 
 }
